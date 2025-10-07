d49e56a7c45d0242acaba8c1b073c97f01615654
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-10-07 16:03:01 -0600

    got rid of wrong Contacts folder

--- Assignment1/Assignment1/obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/Assignment1/obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30fbf59459f7ec512c886a4a1b7daca64b74764f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d66b7cd58e66f118f026a77d994e463a14f5bdfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
